cabed65d2baaf9ec1593fb4cc8635eb452ac53c2
Brandon Tran <bqtran2@wisc.edu>
2024-06-11 10:12:06 -0400

    Setup script to scale rodinia kernel benchmarks

---
+++ rodinia_scale_iterations.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ITERS=${1}
+#change the UINT64 to what you entered
+grep "UINT64_MAX" -rl "src/cuda/rodinia/3.1/cuda/" | xargs sed -i "s/UINT64_MAX/${ITERS}/g"
+
+#Recompile
+make rodinia-3.1_hw_power -C src
+
+#Rename the benchmarks
+mv $BINDIR/release/backprop_k1 $BINDIR/release/backprop_k1_${ITERS}iter
+mv $BINDIR/release/backprop_k2 $BINDIR/release/backprop_k2_${ITERS}iter
+mv $BINDIR/release/btree_k2 $BINDIR/release/btree_k2_${ITERS}iter
+mv $BINDIR/release/btree_k1 $BINDIR/release/btree_k1_${ITERS}iter
+mv $BINDIR/release/hotspot_k1 $BINDIR/release/hotspot_k1_${ITERS}iter
+mv $BINDIR/release/kmeans_k1 $BINDIR/release/kmeans_k1_${ITERS}iter
+mv $BINDIR/release/pathfinder_k1 $BINDIR/release/pathfinder_k1_${ITERS}iter
+mv $BINDIR/release/srad_v1_k1 $BINDIR/release/srad_v1_k1_${ITERS}iter
+ 
+#Generate the config file
+# sed "s|REPLACEITER|${ITERS}|g" "config_rodinia_kernel_template.cfg" > "../flop_counting/config_rodinia_kernel_${ITERS}.cfg"
+
+#Restore the benchmarks back again
+git restore "src/cuda/rodinia/3.1/cuda/*"
