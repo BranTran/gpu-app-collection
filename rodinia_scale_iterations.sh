d2d30e5105e05b3418fc8927ceb4e677f4555fc9
Brandon Tran <bqtran2@wisc.edu>
2024-06-12 09:27:05 -0400

    remove config generation from scaling script

--- rodinia_scale_iterations.sh
+++ rodinia_scale_iterations.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <iterations>"
+    exit 1
+fi
 
 ITERS=${1}
 #change the UINT64 to what you entered
 grep "UINT64_MAX" -rl "src/cuda/rodinia/3.1/cuda/" | xargs sed -i "s/UINT64_MAX/${ITERS}/g"
 
 #Recompile
-make rodinia-3.1_hw_power -C src
+make rodinia-3.1_hw_power -C src -j8
 
 #Rename the benchmarks
 mv $BINDIR/release/backprop_k1 $BINDIR/release/backprop_k1_${ITERS}iter
