ec7e7ea6cfc7d6c7b8a4ff44816eb7f282cc9fd0
Brandon Tran <bqtran2@wisc.edu>
2024-07-12 10:06:34 -0400

    fix scaling and focus on rodinia for now

--- scale_iterations.sh
+++ scale_iterations.sh
@@ -4,13 +4,12 @@ usage() {
     echo "Usage: $0 <iterations>"
     exit 1
 }
-path='accelwattch_validation_one_kernel/rodinia-3.1'
-make_target='_one_kernel'
+path='accelwattch_validation_one_kernel'
 onek='_one_kernel'
 
 ITERS=${1}
 #change the UINT64 to what you entered
-grep "UINT64_MAX" -rl "src/cuda/${path}/cuda/" | xargs sed -i "s/UINT64_MAX/${ITERS}/g"
+grep "UINT64_MAX" -rl "src/cuda/${path}" | xargs sed -i "s/UINT64_MAX/${ITERS}/g"
 
 #Recompile
 make bt_val -C src
@@ -24,8 +23,6 @@ for file in $BINDIR/release/*${onek}; do
   mv "$file" "$BINDIR/release/${base_name}_${ITERS}iter"
 done
  
-#Generate the config file
-# sed "s|REPLACEITER|${ITERS}|g" "config_rodinia_kernel_template.cfg" > "../flop_counting/config_rodinia_kernel_${ITERS}.cfg"
 
 #Restore the benchmarks back again
-git restore "src/cuda/${path}/cuda/*"
+git restore "src/cuda/${path}/"
